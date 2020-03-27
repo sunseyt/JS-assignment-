8eba86bdbfed7c52cf7b853a8737c92b9ce1448b
sunseyt <61967000+sunseyt@users.noreply.github.com>
2020-03-27 11:39:46 +0100

    Add files via upload

---
+++ Firstwork.js
@@ -0,0 +1,13 @@
+let name = 'Richard';
+console.log ('My name is', name );
+
+let myCourse = ['HTML', 'Java Script', 'CSS', 'Python'];
+console.log ('My  courses are', myCourse)
+
+const evendata = [];
+for(i = 1; i <= 200; i++) {
+    if (i%2 == 0) {
+        evendata.push (i);
+    }
+}
+console.log (evendata);
